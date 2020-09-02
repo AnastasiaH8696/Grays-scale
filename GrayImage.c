6335477e5537d8215ac17387b28fac3d0db613fe
Anastasia <nasthg96@gmail.com>
2020-09-02 18:13:35 +0300

    Adding prototypes

---
+++ GrayImage.c
@@ -0,0 +1,15 @@
+/***** INCLUDES *****/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "GrayImage.h"
+
+/***** DEFINES *****/
+
+/***** TYPEDEFS *****/
+
+/******************* Static Function Prototypes *******************/
+
+/******************* Function Implementation *******************/
