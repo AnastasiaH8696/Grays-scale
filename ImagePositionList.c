834bc05abf639cc76fdba8ea107f57dbca8270ea
Anastasia <nasthg96@gmail.com>
2020-10-03 10:06:34 +0300

    Updating Q2 - sorting the segment

--- ImagePositionList.c
+++ ImagePositionList.c
@@ -18,6 +18,10 @@
 static imgPosCell* sortNeighbors(Segment* segment, PIXEL*** flag);
 static void addSegmentToArray(Segment* segment, imgPosCell*** segments, 
 	uint size, uint segmentSize);
+static void addToBeginningOfList(imgPosCell** nodes, imgPos* position);
+static void bubbleSort(imgPosCell* nodes);
+static BOOL isBigger(imgPosCell* maxNode, imgPosCell* minNode);
+static void swap(imgPosCell* maxNode, imgPosCell* minNode);
 
 /******************* Function Implementation *******************/
 
@@ -38,7 +42,7 @@ uint findAllSegments(grayImage* img, unsigned char threshold,
 		/*Finding segment and adding it to the imgPosList sorted by location*/
 		findMinKernel(kernel, img, flag);
 		minSegment = findSingleSegment(img, kernel, threshold);
-		curr = sortNeighbors(minSegment, flag); //TODO: Sort the segment by the locations and create ImgPosCell list
+		curr = sortNeighbors(minSegment, flag); 
 		segmentSize = minSegment->size;
 
 		/*Adding the segment to the array*/
@@ -49,9 +53,79 @@ uint findAllSegments(grayImage* img, unsigned char threshold,
 	return size;
 }
 
-imgPosCell* sortNeighbors(Segment* segment, PIXEL*** flag)
+static imgPosCell* sortNeighbors(Segment* segment, PIXEL*** flag)
 {
-	//TODO
+	imgPosCell* nodes;
+	ushort i, size = segment->size;
+
+	addToBeginningOfList(&nodes, segment->root->position);
+	for (i = 1; i < size; i++)
+		addToBeginningOfList(&nodes, segment->root->similar_neighbors[i-1]->position);
+	
+	bubbleSort(nodes);
+
+	return nodes;
+}
+
+static void addToBeginningOfList(imgPosCell** nodes, imgPos* position, PIXEL*** flag)
+{
+	imgPosCell* node = (imgPosCell*)malloc(sizeof(imgPosCell));
+	checkMemory(node);
+
+	node->position[0] = position[0];
+	node->position[1] = position[1];
+	node->next = *nodes;
+	*nodes = node;
+	node->prev = NULL;
+
+	flag[node->position[0]][node->position[1]] = 1;
+}
+
+static void bubbleSort(imgPosCell *nodes)
+{
+	BOOL isSorted = FALSE;
+	imgPosCell* curr = nodes;
+	imgPos tmp;
+	while (!isSorted)
+	{
+		isSorted = TRUE;
+		curr = nodes;
+		while (curr->next != NULL)
+		{
+			if (isBigger(curr->position, curr->next->position))
+			{
+				isSorted = FALSE;
+				swap(curr, curr->next);
+			}
+			curr = curr->next;
+		}
+	}
+}
+
+static BOOL isBigger(imgPosCell* maxNode, imgPosCell* minNode)
+{
+	BOOL flag = FALSE;
+
+	if (maxNode->position[0] > minNode->position[0])
+		flag = TRUE;
+	else if ((maxNode->position[0] == minNode->position[0])
+		&& maxNode->position[1] > minNode->position[1])
+		flag = TRUE;
+
+	return flag;
+}
+
+static void swap(imgPosCell* maxNode, imgPosCell* minNode)
+{
+	imgPos tmp;
+	tmp[0] = maxNode->position[0];
+	tmp[1] = maxNode->position[1];
+
+	maxNode->position[0] = minNode->position[0];
+	maxNode->position[1] = minNode->position[1];
+
+	minNode->position[0] = tmp[0];
+	minNode->position[1] = tmp[1];
 }
 
 static void addSegmentToArray(Segment* segment, imgPosCell*** segments,
