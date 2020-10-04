cba9ec26c8183fd5a0c7309038d9a181062e39da
Ran Dayan <Ran.Dayan@argus-sec.com>
2020-10-04 18:48:00 +0300

    trying solving section 1

--- Segment.c
+++ Segment.c
@@ -15,7 +15,31 @@
 /******************* Static Function Prototypes *******************/
 
 /******************* Function Implementation *******************/
+
 Segment* findSingleSegment(grayImage* img, imgPos kernel, uchar threshold)
 {
-	//TODO
+	Segment res_segment;
+	addSimilarNeighbors(img, kernel, threshold, res_segment);
+	return &res_segment;
+}
+
+void addSimiliarNeighbors(grayImage* img, imgPos pos, unsigned char threshold, Segment seg)
+{
+	addSimiliarNeighborsRec(img, pos, threshold, seg.root);
+}
+
+void addSimiliarNeighborsRec(grayImage* img, imgPos pos, unsigned char threshold, treeNode* node)
+{
+	if (isCovered(pos) || !inThreshold(pos))
+		return;
+
+	for (unsigned int i = -1; i <= 1; i++)
+	{
+		for (unsigned int j = -1; j <= 1; j++)
+		{
+			pos[ROWS] += i;
+			pos[COLS] += j;
+			addSimilarNeighborsRec();
+		}
+	}
 }
