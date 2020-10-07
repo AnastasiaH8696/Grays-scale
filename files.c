3d6a8a70908779a71f70908fbb490ab15a32f749
Anastasia <nasthg96@gmail.com>
2020-10-07 17:46:58 +0300

    update Q5

--- files.c
+++ files.c
@@ -22,7 +22,7 @@ void saveCompressed(char* fileName, grayImage* img, uchar reducedGrayLevels)
 	ushort pow = findPow(reducedGrayLevels);
 
 	/*The formula: All the pixels * pow(size of each pixel) / 8(size of each byte) */
-	ushort size = ((img->cols * img->rows) * pow) / 8;
+	ushort size = ((img->cols * img->rows) * pow) / BYTE_SIZE;
 	BYTE* compressed; 
 	compressed = compress(img, reducedGrayLevels, size, pow);
 
@@ -60,7 +60,8 @@ static ushort findPow(uchar reducedGrayLevels)
 
 static BYTE* compress(grayImage* img, uchar reducedGrayLevels, ushort size, ushort bitSize)
 {
-	ushort i, j;
+	ushort i, j, freeSpace = BYTE_SIZE, byteIndex = 0;
+	ushort div = (MAX_PIXEL + 1) / reducedGrayLevels; /*The right div for finding compressed value*/
 	uchar currPixel;
 
 	BYTE* compressed = (BYTE*)calloc(size, sizeof(BYTE));
@@ -70,9 +71,22 @@ static BYTE* compress(grayImage* img, uchar reducedGrayLevels, ushort size, usho
 	{
 		for (j = 0; j < img->cols; j++)
 		{
-			currPixel = img->pixels[i][j];
-			//TODO
-
+			currPixel = (img->pixels[i][j]) / div;
+			if (freeSpace < bitSize) /* The number between two bytes */
+			{
+				compressed[byteIndex] |= currPixel >> (bitSize - freeSpace);
+				byteIndex++;
+				compressed[byteIndex] |= currPixel << (BYTE_SIZE-bitSize) + (bitSize - freeSpace);
+				freeSpace = (BYTE_SIZE - bitSize) + (bitSize - freeSpace);
+			}
+			else
+			{
+				compressed[byteIndex] |= currPixel << freeSpace - bitSize;
+				freeSpace = freeSpace - bitSize;
+			}
+			
 		}
 	}
+
+	return compressed;
 }
