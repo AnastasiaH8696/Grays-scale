691b57354d49a917d7bf0261861ebaed5b890a79
Ran Dayan <Ran.Dayan@argus-sec.com>
2020-10-10 21:29:51 +0300

    Reading PGM files

--- files.c
+++ files.c
@@ -10,6 +10,7 @@
 
 #include "definitions.h"
 #include "files.h"
+#include "GrayImage.h"
 
 /***** TYPEDEFS *****/
 
@@ -19,6 +20,59 @@ static ushort findPow(uchar reducedGrayLevels);
 static BYTE* compress(grayImage* img, uchar reducedGrayLevels, ushort size, ushort bitSize);
 
 /******************* Function Implementation *******************/
+grayImage* readPGM(char* fname)
+{
+	grayImage res_grayImage;
+	FILE* pgmFile;
+	char version[3];
+	ushort maxVal;
+	ushort row, col;
+	int val;
+	pgmFile = fopen(fname, "rb");
+	if (pgmFile == NULL) {
+		fprintf(stderr, "Cannot open the file");
+		exit(OPEN_FILE_ERROR);
+	}
+	fgets(version, sizeof(version), pgmFile);
+	if (strcmp(version, "P5")) {
+		fprintf(stderr, "Wrong file version!\n");
+		exit(FILE_VERSION_ERROR);
+	}
+	skipComments(pgmFile);
+	fscanf(pgmFile, "%d", res_grayImage.cols);
+	skipComments(pgmFile);
+	fscanf(pgmFile, "%d", res_grayImage.rows);
+	skipComments(pgmFile);
+	fscanf(pgmFile, "%d", maxVal);
+	fgetc(pgmFile);
+
+	res_grayImage.pixels = createEmptyImg(res_grayImage.rows, res_grayImage.cols);
+	for (row = 0; row < res_grayImage.rows; row++) {
+		for (col = 0; col < res_grayImage.cols; col++) {
+			fscanf(pgmFile, "%d", &val);
+			res_grayImage.pixels[row][col] = (int)(((double)val / (double)maxVal) * 255);
+		}
+	}
+
+	fclose(pgmFile);
+	return &res_grayImage;
+}
+
+void skipComments(FILE* fp)
+{
+	int ch;
+	char line[100];
+	while ((ch = fgetc(fp)) != EOF && isspace(ch));
+
+	if (ch == '#') {
+		fgets(line, sizeof(line), fp);
+		skipComments(fp);
+	}
+	else {
+		fseek(fp, -1, SEEK_CUR);
+	}
+}
+
 void saveCompressed(char* fileName, grayImage* img, uchar reducedGrayLevels)
 {
 	ushort pow = findPow(reducedGrayLevels);
