aeefbe672d1ab43226d58af3a47eb4150081b175
Ran Dayan <Ran.Dayan@argus-sec.com>
2020-10-19 20:34:37 +0300

    re-arrange functions titles

--- Segment.h
+++ Segment.h
@@ -15,14 +15,4 @@
 The function returns a pointer to the found segment. */
 Segment* findSingleSegment(grayImage* img, imgPos kernel, uchar threshold);
 
-treeNode** allocateTreeNodeList();
-treeNode* initTreeNode(imgPos rootPos);
-Segment* initSegment(imgPos rootPos);
-void appendChild(treeNode*** tnlst, treeNode* child);
-void appendNeighbor(treeNode* tn, treeNode* neighbor);
-void addChildren(grayImage* img, treeNode* tn, uchar minVal, uchar maxVal, BYTE*** flag, uint* size);
-void buildSegmentRec(grayImage* img, treeNode** tnlst, uchar minVal, uchar maxVal, BYTE*** flag, uint* size);
-void buildSegment(grayImage* img, Segment* seg, uchar threshold, BYTE*** flag);
-
-
 #endif
